c8e7ab573edcac6c38842d3aedf41c648aeb50e3
mattia <mattia.livraghi@gmail.com>
2026-04-02 17:04:00 +0200

    feat(host): add utility to check for CUDA errors
    Uses C++20 <source_location> header to avoid the legacy C macros __FILE__ and __LINE__ .

---
+++ src/io_utils.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+#include <format>
+#include <source_location>
+#include <stdexcept>
+#include <cuda_runtime.h>
+
+// Inline allows us to define this function in a header without violating the ODR
+inline void cudaCheck(cudaError_t err,
+                      std::source_location location = std::source_location::current()) {
+    
+    if (err != cudaSuccess) {
+        std::string msg = std::format("CUDA error at {}:{} code={} \"{}\"\n",
+                                      location.file_name(),
+                                      location.line(),
+                                      static_cast<int>(err),
+                                      cudaGetErrorString(err));
+        throw std::runtime_error(msg);
+    }
+}
