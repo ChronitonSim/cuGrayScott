2be567a1fb6cd017ac39592c64aa55ced4402a45
mattia <mattia.livraghi@gmail.com>
2026-04-04 18:54:49 +0200

    feat(kernel): add utilities to compute Laplacian and explicit Euler step
    Also, refactor getIndex as __host__ __device__ to rule both CPU and GPU.

--- src/utils.hpp
+++ src/utils.hpp
@@ -7,13 +7,45 @@
 // The y dim is indexed by j, so j is the row index.
 // Thus, a node of grid coordinates (j, i) has 
 // linearized coordinate k = j * N_x + i
-inline int getIndex(int col, int row, int width){
+__host__ __device__
+inline int getIndex(const int col, const int row, const int width){
     return row * width + col;
 }
 
 __device__
-inline int d_getIndex(int col, int row, int width){
-    return row * width + col;
+inline float computeLaplacian(
+    const float center,
+    const float south,
+    const float north,
+    const float east,
+    const float west,
+    const float grid_spacing
+) {
+    return (south + north + east + west - 4 * center) / (grid_spacing * grid_spacing);
 }
 
+__device__
+inline float computeExplicitEulerStepU(
+    const float centerU,
+    const float centerV,
+    const float laplacian,
+    const float dt,
+    const float D,
+    const float F
+) {
+    return centerU + dt * (D * laplacian - centerU * centerV * centerV + F * (1 - centerU));
+}
+
+__device__
+inline float computeExplicitEulerStepV(
+    const float centerU,
+    const float centerV,
+    const float laplacian,
+    const float dt,
+    const float D,
+    const float F,
+    const float k
+) {
+    return centerV + dt * (D * laplacian + centerU * centerV * centerV - (F + k) * centerV);
+}
 
